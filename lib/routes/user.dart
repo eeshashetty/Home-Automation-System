cef754600229f38263ad54dc4df589cd7b91cf85
Eesha Shetty <eeshashetty24@gmail.com>
2020-10-14 15:50:33 +0530

    Updated Bluetooth Settings

--- lib/routes/user.dart
+++ lib/routes/user.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 class User extends StatefulWidget {
   @override
@@ -8,15 +14,109 @@ class User extends StatefulWidget {
 
 class _State extends State<User> {
   // This widget is the root of your application.
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  FlutterBluetoothSerial _bluetooth = FlutterBluetoothSerial.instance;
+  BluetoothConnection connection;
+
+  int _deviceState;
+
   bool _enabled = false;
-  bool _connec = false;
-  bool _toggle = false;
+  bool isDisconnecting = false;
+
+  bool get isConnected => connection != null && connection.isConnected;
+
+  List<BluetoothDevice> _devicesList = [];
+  BluetoothDevice _device;
+
+  bool _connected = false;
+  bool _toggle1 = false;
+  bool _toggle2 = false;
+  bool _toggle3 = false;
   int p = 1;
   String dropdownValue = 'Select Device';
 
   @override
+  void init() {
+    FlutterBluetoothSerial.instance.state.then((state) {
+      setState(() {
+        _bluetoothState = state;
+      });
+    });
+
+    _deviceState = 0;
+
+    enableBluetooth();
+
+    FlutterBluetoothSerial.instance.onStateChanged()
+    .listen((BluetoothState state){
+      setState(() {
+        _bluetoothState = state;
+        if(_bluetoothState == BluetoothState.STATE_OFF) {
+          _enabled = false;
+        }
+        getPairedDevices();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // Avoid memory leak and disconnect
+    if (isConnected) {
+      isDisconnecting = true;
+      connection.dispose();
+      connection = null;
+    }
+
+    super.dispose();
+  }
+
+  // Request Bluetooth permission from the user
+  Future<void> enableBluetooth() async {
+    // Retrieving the current Bluetooth state
+    _bluetoothState = await FlutterBluetoothSerial.instance.state;
+
+    // If the bluetooth is off, then turn it on first
+    // and then retrieve the devices that are paired.
+    if (_bluetoothState == BluetoothState.STATE_OFF) {
+      await FlutterBluetoothSerial.instance.requestEnable();
+      await getPairedDevices();
+      return true;
+    } else {
+      await getPairedDevices();
+    }
+    return false;
+  }
+
+  // For retrieving and storing the paired devices
+  // in a list.
+  Future<void> getPairedDevices() async {
+    List<BluetoothDevice> devices = [];
+
+    // To get the list of paired devices
+    try {
+      devices = await _bluetooth.getBondedDevices();
+    } on PlatformException {
+      print("Error");
+    }
+
+    // It is an error to call [setState] unless [mounted] is true.
+    if (!mounted) {
+      return;
+    }
+
+    // Store the [devices] list in the [_devicesList] for accessing
+    // the list outside this class
+    setState(() {
+      _devicesList = devices;
+    });
+  }
+
+
   Widget build(BuildContext context){
     return Scaffold(
+      key: _scaffoldKey,
         body: Padding(
           padding: const EdgeInsets.fromLTRB(20.0,80.0,0,0),
           child: Column(
@@ -59,16 +159,22 @@ class _State extends State<User> {
                 style: Theme.of(context).textTheme.headline1,
           children: <TextSpan>[
                 TextSpan(text: 'Enable Bluetooth\n', style: TextStyle(fontSize: 20),),
-                TextSpan(text: _connec ? 'Connected' : 'Not Connected', style: TextStyle(fontSize: 12, color: _connec ? Colors.green : Colors.red),),
+                TextSpan(text: _connected ? 'Connected' : 'Not Connected', style: TextStyle(fontSize: 12, color: _connected ? Colors.green : Colors.red),),
                 ],
             ),
             ),
 
               Padding(
-                padding: EdgeInsets.only(right: 40),
+                padding: EdgeInsets.only(right: 25),
                 child: Switch(
                   value: _enabled,
-                  onChanged: (value) {
+                  onChanged:  (value) async {
+                    if(_enabled == false)
+                      init();
+                    if(_enabled) {
+                      await FlutterBluetoothSerial.instance
+                          .requestDisable();
+                    }
                     setState(() {
                       _enabled = value;
                     });
@@ -82,44 +188,74 @@ class _State extends State<User> {
             ),
           Padding(
             padding: const EdgeInsets.only(top: 20, right: 20.0),
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.only(right: 20),
-              height: 44,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black45),
-                borderRadius: BorderRadius.circular(9)
-              ),
-              child: DropdownButtonHideUnderline(
-                  child: DropdownButton<String>(
-                    isExpanded: false,
-                    value: dropdownValue,
-                    icon: Icon(Icons.arrow_forward),
-                    iconSize: 15,
-                    elevation: 11,
-                    style: TextStyle(color: Colors.black45),
-                    onChanged: (String newValue) {
-                      setState(() {
-                        dropdownValue = newValue;
-                      });
-                    },
-                    items: <String>['Select Device','One', 'Two', 'Free', 'Four']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Container(
-                          padding: EdgeInsets.all(10),
-                          width: 200,
-                          height: 44,
-                          child: Text(
-                            value,
-                            style: TextStyle(fontFamily: 'Montserrat', fontSize: 15, fontWeight: FontWeight.w400, color: Color(0xFF6A6A6A)),
-                          ),
-                        ),
-                      );
-                    }).toList(),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width*0.75,
+                  padding: EdgeInsets.only(right: 20),
+                  height: 44,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black45),
+                    borderRadius: BorderRadius.circular(9)
                   ),
+                  child: DropdownButtonHideUnderline(
+                      child: DropdownButton<BluetoothDevice>(
+                        items: _getDeviceItems(),
+                        elevation: 16,
+                        onChanged: (value) {
+                          final name = value.name;
+                          // const dialog =
+                          showDialog(
+                              context: context,
+                              builder: (_) =>
+                                  AlertDialog(
+                                    insetPadding: EdgeInsets.all(70),
+                                    contentPadding: EdgeInsets.all(30),
+                                    content: RichText(
+                                      text: new TextSpan(
+                                        style: TextStyle(color: Colors.black87, fontSize: 18),
+                                        children: <TextSpan>[
+                                          TextSpan(text: 'Connect to '),
+                                          TextSpan(text: '$name?', style: TextStyle(fontWeight: FontWeight.bold),),
+                                        ],
+                                      ),
+                                    ),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                          _connect;
+                                          setState(() => _device = value);
+                                          },
+                                        child: Text('YES'),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('NO'),
+                                      ),
+                                    ],
+                                  )
+                          );
+                        },
+                        value: _devicesList.isNotEmpty ? _device : null,
+                        icon: Icon(Icons.arrow_forward),
+                        iconSize: 15,
+                        style: TextStyle(color: Colors.black45),
+                      ),
+                    ),
                 ),
+                IconButton(
+                  icon: Icon(Icons.refresh),
+                  onPressed: () async {
+                    await getPairedDevices().then((value) =>
+                      show('Device List Refreshed'),
+                    );
+                  },
+                )
+              ],
             ),
           ),
             SizedBox(height: 100,),
@@ -128,11 +264,11 @@ class _State extends State<User> {
                   style: Theme.of(context).textTheme.headline2,
                   children: <TextSpan>[
                     new TextSpan(text:
-                    p==0 ? 'Lights: ' :
-                    p==1 ? 'Lamp: '  :
+                    p==1 ? 'Lights: ' :
+                    p==3 ? 'Lamp: '  :
                     p==2 ? 'Fan: ' : 'assets/images/light.png'
                     ),
-                    new TextSpan(text: _toggle ? 'ON' : 'OFF', style: TextStyle(color: _toggle ? Colors.green : Colors.red))
+                    new TextSpan(text: (p==1?_toggle1:p==2?_toggle2:_toggle3) ? 'ON' : 'OFF', style: TextStyle(color: (p==1?_toggle1:p==2?_toggle2:_toggle3) ? Colors.green : Colors.red))
                   ]
               ),
             ),
@@ -145,22 +281,30 @@ class _State extends State<User> {
                   onPressed: () {
                     setState(()
                     {
-                      p = p > 0 ? p - 1 : 2;
+                      p = p > 1 ? p - 1 : 3;
                       print(p);
                     }
                     );
                   },
                 ),
                 MaterialButton(
-                  onPressed: () { setState(() {
-                    _toggle = _toggle ? false : true ; print(_toggle);
-                  });},
+                  onPressed: () {
+                    print('Sending $p');
+                    _sendMessage(p);
+                    setState(() {
+                    if(p==1)
+                      _toggle1 = !_toggle1;
+                    if(p==2)
+                      _toggle2 = !_toggle2;
+                    if(p==3)
+                      _toggle3 = !_toggle3;
+                    });},
                   child: Container(
                     height: 193,
                     width: 179,
                     decoration: BoxDecoration(
                       gradient: LinearGradient(
-                        colors: [ _toggle ? Color(0xff191970) : Colors.black12, _toggle ? Color(0xff4A4A8E) : Colors.black12] ,
+                        colors: [ (p==1?_toggle1:p==2?_toggle2:_toggle3) ? Color(0xff191970) : Colors.black12, (p==1?_toggle1:p==2?_toggle2:_toggle3) ? Color(0xff4A4A8E) : Colors.black12] ,
                         begin: Alignment.centerLeft,
                         end: Alignment.centerRight,
                       ),
@@ -170,13 +314,13 @@ class _State extends State<User> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Image(image: AssetImage(
-                          p==0 ? 'assets/images/light.png' :
-                          p==1 ? 'assets/images/lamp.png'  :
+                          p==1 ? 'assets/images/light.png' :
+                          p==3 ? 'assets/images/lamp.png'  :
                           p==2 ? 'assets/images/fan.png' : 'assets/images/light.png'
                         ), width: 100),
                         SizedBox(height: 15,),
-                        Text(  p==0 ? 'Lights' :
-                        p==1 ? 'Lamp'  :
+                        Text(  p==1 ? 'Lights' :
+                        p==3 ? 'Lamp'  :
                         p==2 ? 'Fan' : 'assets/images/light.png'
                         , style: Theme.of(context).textTheme.bodyText1),
                       ],
@@ -188,7 +332,7 @@ class _State extends State<User> {
                   onPressed: () {
                     setState(()
                     {
-                      p = p < 2 ? p + 1 : 0;
+                      p = p < 3 ? p + 1 : 1;
                       print(p);
                     }
                     );
@@ -201,4 +345,82 @@ class _State extends State<User> {
         ),
     );
   }
+  void _sendMessage(code) async {
+    connection.output.add(utf8.encode("$code"+"\r\n"));
+    await connection.output.allSent;
+    show('Sent $code');
+  }
+
+  void _connect() async {
+    setState(() {
+      _connected = false;
+    });
+    if (_device == null) {
+      show('No device selected');
+    } else {
+      if (!isConnected) {
+        await BluetoothConnection.toAddress(_device.address)
+            .then((_connection) {
+          show('Connected to Device!');
+          connection = _connection;
+          setState(() {
+            _connected = true;
+          });
+
+          connection.input.listen(null).onDone(() {
+            if (isDisconnecting) {
+              print('Disconnecting locally!');
+            } else {
+              print('Disconnected remotely!');
+            }
+            if (this.mounted) {
+              setState(() {});
+            }
+          });
+        }).catchError((error) {
+          show('Cannot connect, exception occurred');
+          print(error);
+        });
+
+      }
+    }
+  }
+
+  List<DropdownMenuItem<BluetoothDevice>> _getDeviceItems() {
+    List<DropdownMenuItem<BluetoothDevice>> items = [];
+    if (_devicesList.isEmpty) {
+      items.add(DropdownMenuItem(
+        child: Padding(
+          padding: const EdgeInsets.only(left:20.0),
+          child: Text('No Devices'),
+        ),
+      ));
+    } else {
+      _devicesList.forEach((device) {
+        items.add(DropdownMenuItem(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+              child: Text(device.name)
+            ),
+          value: device,
+          ));
+      });
+    }
+    return items;
+  }
+
+Future show(
+    String message, {
+      Duration duration: const Duration(seconds: 3),
+    }) async {
+  await new Future.delayed(new Duration(milliseconds: 100));
+  _scaffoldKey.currentState.showSnackBar(
+    new SnackBar(
+      content: new Text(
+        message,
+      ),
+      duration: duration,
+    ),
+  );
+}
 }
